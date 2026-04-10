b0fa4a31a3e9e849cdca6c8a593e4c1615a4af86
Nathan Freitas <nrodriguesdefreitas@gmail.com>
2026-04-10 09:43:54 -0300

    fix: updates firebase import paths

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { FirebaseModule } from 'src/firebase/firebase.module';
+import { FirebaseModule } from '../../firebase/firebase.module';
 
 @Module({
   imports: [FirebaseModule],
